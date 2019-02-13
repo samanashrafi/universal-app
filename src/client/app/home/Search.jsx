13892f73da6ec8a8b457eca148ad21af9e65cdcb
Saman Ashrafi <saman.ashrafi10@gmail.com>
2019-02-13 15:16:42 +0330

    add art in Search.jsx

--- src/client/app/home/Search.jsx
+++ src/client/app/home/Search.jsx
@@ -3,6 +3,8 @@ import {connect} from 'react-redux';
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { searchFetch} from 'src/redux/actions/search-action.js'
+import SelectFieldGroup from "client/app/common/SelectFieldGroup"
+import TextFieldGroup from "client/app/common/TextFieldGroup"
 import {
   withRouter
 } from 'react-router-dom'
@@ -10,37 +12,117 @@ class Search extends Component {
   constructor(props){
     super(props);
     this.state ={
-      locale:"",
-      category:"",
+      text:"",
+      art:"",
+      artHeader:"هنر و موسیقی",
+      artList:[
+        {
+          id: "1",
+          title: "هنر",
+          selected:false,
+          key:"artList"
+
+        },
+        {
+          id: 2,
+          title: "موسیقی",
+          selected:false,
+          key:"artList"
+        }
+      ],
+
+      categoryArt:"",
+      categoryArtHeader:"زمینه هنری",
+      categoryArtList:[
+        {
+          id: "1",
+          title: "مبانی ترکیب بندی در عکاسی",
+          selected:false,
+          key:"categoryArtList"
+
+        },
+        {
+          id: 2,
+          title: "سفال و سرامیک",
+          selected:false,
+          key:"categoryArtList"
+        }
+      ],
+      city:"",
+      cityHeader:"شهر",
+      cityList:[
+        {
+          id: "1",
+          title: "تهران",
+          selected:false,
+          key:"cityList"
+
+        },
+        {
+          id: 2,
+          title: "کرج",
+          selected:false,
+          key:"cityList"
+        },
+        {
+          id: 3,
+          title: "اصفهان",
+          selected:false,
+          key:"cityList"
+        },
+        {
+          id: 4,
+          title: "بندرعباس",
+          selected:false,
+          key:"cityList"
+        }
+      ],
+
+      neighbourhood:"",
+      neighbourhoodHeader:"منطقه و محله",
+      neighbourhoodList:[
+        {
+          id: "1",
+          title: "منطقه 1 - آجودانیه",
+          selected:false,
+          key:"neighbourhoodList"
+
+        },
+        {
+          id: 2,
+          title: "منطقه 1 - آصف",
+          selected:false,
+          key:"neighbourhoodList"
+        }
+       
+      ],
+      errors:[],
       input:0
     }
-   this.onChangeCategory = this.onChangeCategory.bind(this);
-   this.onChangeLocale = this.onChangeLocale.bind(this);
-   this.onChangeQ = this.onChangeQ.bind(this);
+   this.toggleSelected = this.toggleSelected.bind(this);
+   this.onChange = this.onChange.bind(this);
+
+  //  this.onChangeQ = this.onChangeQ.bind(this);
    this.onSubmit = this.onSubmit.bind(this);
 
   }
-  onChangeCategory(e){
-    this.setState({category:e.target.value})
+  onChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
   }
-  onChangeLocale(e){
-    this.setState({locale:e.target.value})
-  }
-  filterText(filter){
-     return filter.replace(/ی/g,'ي').replace(/ک/g,'ك');
-  }
-  onChangeQ(e){
-    e.preventDefault();
-
-     this.setState({input:this.refs.searchFilter.value.length})
-     
-    if(this.refs.searchFilter.value.length > 2){
-
-      this.props.searchFetch(this.filterText(this.refs.searchFilter.value));
-    }else{
-      this.refs.searchFilter.focus();
-    }
-  }
+  // filterText(filter){
+  //    return filter.replace(/ی/g,'ي').replace(/ک/g,'ك');
+  // }
+  // onChangeQ(e){
+  //   e.preventDefault();
+  //   this.setState({input:this.refs.searchFilter.value.length})
+  //   if(this.refs.searchFilter.value.length > 2){
+  //     this.props.searchFetch(this.filterText(this.refs.searchFilter.value));
+  //   }else{
+  //     this.refs.searchFilter.focus();
+  //   }
+  // }
 
   onSubmit(e){
     e.preventDefault();
@@ -54,9 +136,27 @@ class Search extends Component {
       }
     }
   }
+
+  toggleSelected(id, key, state) {
+    var temp = this.state[key].map(obj => {
+      var rObj = {};
+      rObj["id"] = obj.id;
+      rObj["title"] = obj.title;
+      rObj["selected"] = false;
+      rObj["key"] = obj.key;
+      return rObj;
+    });
+    temp[id].selected = !temp[id].selected;
+    this.setState({
+      [key]: temp,
+      [state]: temp[id].title
+    });
+
+  }
   
   render() {
     const {data,isLoaded} = this.props;
+    const { text,art,artList,artHeader,categoryArt,categoryArtHeader,categoryArtList,city,cityHeader,cityList,neighbourhood,neighbourhoodHeader,neighbourhoodList,errors } = this.state
     let searchList;
     if(isLoaded){
        searchList = data.map( d =>{
@@ -71,39 +171,103 @@ class Search extends Component {
     return (
       <div className="home-search">
         <div className="container-center">
-          <form className="filter" onSubmit={this.onSubmit}>
+        <div className="position-relative-margin">
+            <div className="category">
+                <li>
+                  <div className="icon"></div>
+                  <span>هنر و موسیقی</span>
+                </li>
+                <li>
+                  <div className="icon"></div>
+                  <span>آموزشگاه زبان</span>
+                </li>
+                <li>
+                  <div className="icon"></div>
+                  <span>مدارس غیر انتفاعی</span>
+                </li>
+                <li>
+                  <div className="icon"></div>
+                  <span>آموزشگاه های علمی</span>
+                </li>
+               
+                <li>
+                  <div className="icon"></div>
+                  <span>فنی و حرفه ای</span>
+                </li>
+                
+                <li>
+                  <div className="icon"></div>
+                  <span>طبیعت و گردشگری</span>
+                </li>
+            </div>
+        </div>
+          <form className="filter art" onSubmit={this.onSubmit}>
             <div className="form-select">
-              <input type="search"  ref="searchFilter" onChange={this.onChangeQ} placeholder="لطفا مطلب خود را جستجو کنید..." />
-              { this.state.input > 2 && searchList?
-              <ul className="search-list">
-                {searchList.length == 0 ? 
-                  <li className="not-found">موردی یافت نشد!!!</li>
-              :searchList }
-              </ul>
-              :""}
-              
+                <SelectFieldGroup
+                  title={art}
+                  headerDefalt={artHeader}
+                  state={"art"}
+                  list={artList}
+                  toggleItem={this.toggleSelected}
+                  icon={""}
+                  error={errors.art}
+                  isLoaded={true}
+                />
             </div>
             <div className="form-select">
-              <select value={this.state.category} onChange={this.onChangeCategory}>
-                <option>مدارس</option>
-                <option>آموزشکده</option>
-                <option>موسسات</option>
-                <option>سالن های ورزشی و تفریحی</option>
-              </select>
+              <SelectFieldGroup
+                  title={categoryArt}
+                  headerDefalt={categoryArtHeader}
+                  state="categoryArt"
+                  list={categoryArtList}
+                  toggleItem={this.toggleSelected}
+                  icon={""}
+                  error={errors.categoryArt}
+                  isLoaded={true}
+                />
             </div>
             <div className="form-select">
-            <select value={this.state.locale} onChange={this.onChangeLocale}>
-                <option>صادقیه</option>
-                <option>پونک</option>ٍ
-                <option>استاد معین</option>
-                <option>نیاوران</option>
-              </select>
+              <SelectFieldGroup
+                  title={city}
+                  headerDefalt={cityHeader}
+                  state="city"
+                  list={cityList}
+                  toggleItem={this.toggleSelected}
+                  icon={""}
+                  error={errors.city}
+                  isLoaded={true}
+                />
             </div>
-            <button className="btn"><span>جستجو</span>
+            <div className="form-select">
+              <SelectFieldGroup
+                  title={neighbourhood}
+                  headerDefalt={neighbourhoodHeader}
+                  state="neighbourhood"
+                  list={neighbourhoodList}
+                  toggleItem={this.toggleSelected}
+                  icon={""}
+                  error={errors.neighbourhood}
+                  isLoaded={true}
+                />
+            </div>
+            <div className="form-select">
+               <TextFieldGroup
+                  type="text"
+                  name="text"
+                  label="آموزشگده یا دوره مورد نظر..."
+                  value={text}
+                  onChange={this.onChange}
+                  error={errors.text}
+                  icon={"k-edit"}
+                />
+            </div>
+            <button className="btn">
+              <span>جستجو</span>
               <div className="spinner">
                 <div className="double-bounce1" />
                 <div className="double-bounce2" />
-              </div></button>
+              </div>
+            </button>
           </form>
         </div>
       </div>
