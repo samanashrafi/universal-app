77293a7ee62876fae376b09eedcf9247be768b35
Saman Ashrafi <saman.ashrafi10@gmail.com>
2019-02-16 15:18:39 +0330

    complate responsive art layout

--- src/client/app/home/Search.jsx
+++ src/client/app/home/Search.jsx
@@ -1,82 +1,83 @@
 import React, { Component } from "react";
-import {connect} from 'react-redux';
+import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-import $ from "jquery"
-import { searchFetch} from 'src/redux/actions/search-action.js'
+import $ from "jquery";
+import { searchFetch } from "src/redux/actions/search-action.js";
 
-import ArtMusic from 'client/app/home/ArtMusic.jsx';
-import Language from 'client/app/home/Language.jsx';
+import ArtMusic from "client/app/home/ArtMusic.jsx";
+import Language from "client/app/home/Language.jsx";
 
-import {
-  withRouter
-} from 'react-router-dom'
+import { withRouter } from "react-router-dom";
 import swal from "sweetalert";
 class Search extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state ={
-     artMusicLoaded:false,
-     languageLoaded:false
-    }
-   this.selectCategory = this.selectCategory.bind(this);
-
+    this.state = {
+      artMusicLoaded: false,
+      languageLoaded: false
+    };
+    this.selectCategory = this.selectCategory.bind(this);
   }
-  selectCategory(e){
-    let title = e.currentTarget.innerText
-    switch(title.trim()){
+  selectCategory(e) {
+    let title = e.currentTarget.innerText;
+    switch (title.trim()) {
       case "هنر و موسیقی":
-      return this.setState({
-        artMusicLoaded:true,
-        languageLoaded:false
-      })
+        return this.setState({
+          artMusicLoaded: true,
+          languageLoaded: false
+        });
       case "آموزشگاه زبان":
-      return this.setState({
-        artMusicLoaded:false,
-        languageLoaded:true
-      })
+        return this.setState({
+          artMusicLoaded: false,
+          languageLoaded: true
+        });
       default:
-      ""
+        "";
     }
   }
   render() {
-    const { artMusicLoaded,languageLoaded } = this.state
+    const { artMusicLoaded, languageLoaded } = this.state;
     return (
       <div className="home-search">
-        <div className="container-center">
-        <div className="position-relative-margin" >
-            <div className="category" >
-                <li onClick={this.selectCategory} >
-                  <div className="icon"></div>
+        <div className="con">
+          <div className="container-center">
+            <div className="cat-list position-relative-margin">
+              <ul className="category">
+                <li onClick={this.selectCategory}>
+                  <div className="icon" />
                   <span>هنر و موسیقی</span>
                 </li>
-                <li onClick={this.selectCategory} >
-                  <div className="icon"></div>
+                <li onClick={this.selectCategory}>
+                  <div className="icon" />
                   <span>آموزشگاه زبان</span>
                 </li>
                 <li onClick={this.selectCategory}>
-                  <div className="icon"></div>
+                  <div className="icon" />
                   <span>مدارس غیر انتفاعی</span>
                 </li>
                 <li onClick={this.selectCategory}>
-                  <div className="icon"></div>
+                  <div className="icon" />
                   <span>آموزشگاه های علمی</span>
                 </li>
-               
                 <li onClick={this.selectCategory}>
-                  <div className="icon"></div>
+                  <div className="icon" />
                   <span>فنی و حرفه ای</span>
                 </li>
-                
                 <li onClick={this.selectCategory}>
-                  <div className="icon"></div>
+                  <div className="icon" />
                   <span>طبیعت و گردشگری</span>
                 </li>
+              </ul>
             </div>
+          </div>
         </div>
+        <div className="container-center">
         {artMusicLoaded ? <ArtMusic/> : null }
-        {languageLoaded ? <Language/> : null }
+          {languageLoaded ? <Language/> : null }
         </div>
+
+      
       </div>
     );
   }
@@ -87,11 +88,12 @@ class Search extends Component {
 //   // isLoaded: PropTypes.bool.isRequired
 // };
 
-const mapStateToProps = state =>( {
-  data : state.search.data,
+const mapStateToProps = state => ({
+  data: state.search.data,
   isLoaded: state.search.isLoaded
 });
 
-
- export default connect(mapStateToProps,{searchFetch})(withRouter(Search));
-
+export default connect(
+  mapStateToProps,
+  { searchFetch }
+)(withRouter(Search));
