56169ca680059fbd9bcac68bb74cdfad883af3a4
saman <saman.ashrafi10@gmail.com>
2019-02-22 21:25:55 +0330

    [Task] Create Login.jsx & Register.jsx

--- src/client/app/user/Login-Register.jsx
+++ src/client/app/user/Login-Register.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import Login from "client/app/user/Login.jsx";
+import Register from "client/app/user/Register.jsx";
 
 class LoginRegister extends Component {
   render() {
@@ -8,6 +10,8 @@ class LoginRegister extends Component {
           <div className="boxes">
             <form className="form">
               <h2>ورود یا عضویت</h2>
+              <Login />
+              <Register />
             </form>
             <div className="cover" />
           </div>
---
+++ src/client/app/user/Login.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class Login extends Component {
+  render() {
+    return <form>Login</form>;
+  }
+}
+export default Login;
