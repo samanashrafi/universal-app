db87503c43f8dcd5a01d42595ebcdd739db5a0f4
saman <saman.ashrafi10@gmail.com>
2019-02-23 22:35:48 +0330

    [Task] Add server resolve path in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,98 +1,99 @@
-const path = require('path'),
-    webpack = require('webpack'),
-    distPath = path.resolve(__dirname, 'dist'),
-    srcPath = path.resolve(__dirname, 'src/client'),
+const path = require("path"),
+  webpack = require("webpack"),
+  distPath = path.resolve(__dirname, "dist"),
+  srcPath = path.resolve(__dirname, "src/client"),
+  ExtractTextPlugin = require("extract-text-webpack-plugin"),
+  HtmlWebPackPlugin = require("html-webpack-plugin");
 
-    ExtractTextPlugin = require("extract-text-webpack-plugin"),
-    HtmlWebPackPlugin = require('html-webpack-plugin')
+const production =
+  process.env.NODE_ENV && process.env.NODE_ENV === "production";
 
-const production = process.env.NODE_ENV &&
-    process.env.NODE_ENV === "production";
-
-const Dotenv = require('dotenv-webpack');
+const Dotenv = require("dotenv-webpack");
 if (!production) {
-    require('dotenv').config();
+  require("dotenv").config();
 }
 
-const modeWebpack = production ?
-    "production" :
-    "development";
+const modeWebpack = production ? "production" : "development";
 
 module.exports = {
-    // context: srcPath,
-    entry: './src/client/index.js',
-    output: {
-
-        filename: 'client.js',
-        path: distPath,
-        publicPath: '/dist/',
-
-    },
-    devServer: {
-        contentBase: srcPath,
-        port: 3000,
-        open: true,
-        historyApiFallback: true
-    },
-    mode: modeWebpack,
-    module: {
-        rules: [{
-                test: /\.(js|jsx)$/,
-                exclude: [/node_modules/],
-                use: [{
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['es2015', 'react'],
-                        plugins: ['transform-object-rest-spread', 'async-to-promises']
-                    }
-                }],
-            }, {
-                test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: ["css-loader", "sass-loader"]
-                })
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: ["file-loader"]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: ["file-loader"]
+  // context: srcPath,
+  entry: "./src/client/index.js",
+  output: {
+    filename: "client.js",
+    path: distPath,
+    publicPath: "/dist/"
+  },
+  devServer: {
+    contentBase: srcPath,
+    port: 3000,
+    open: true,
+    historyApiFallback: true
+  },
+  mode: modeWebpack,
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: [/node_modules/],
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["es2015", "react"],
+              plugins: ["transform-object-rest-spread", "async-to-promises"]
             }
-            //loaders for other file types can go here
+          }
         ]
-    },
-    plugins: [
-        new Dotenv(),
-        new ExtractTextPlugin({
-            filename: "app.css"
-        }),
-        new HtmlWebPackPlugin({
-            template: "./src/client/index.html",
-            filename: "index.html"
-        }),
-        new webpack.ProvidePlugin({
-            'React': 'react',
-            '$': 'jquery',
-            'ReactDOM': 'react-dom',
-
-        }),
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
-            'process.env.BROWSER': JSON.stringify(true),
-        }),
-    ],
-    resolve: {
-        alias: {
-            src: path.resolve(__dirname, "src"),
-            assets: path.resolve(__dirname, "src/assets"),
-            client: path.resolve(__dirname, "src/client"),
-        }
-    },
-    node: {
-        module: 'empty',
-        fs: 'empty'
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: ["css-loader", "sass-loader"]
+        })
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"]
+      }
+      //loaders for other file types can go here
+    ]
+  },
+  plugins: [
+    new Dotenv(),
+    new ExtractTextPlugin({
+      filename: "app.css"
+    }),
+    new HtmlWebPackPlugin({
+      template: "./src/client/index.html",
+      filename: "index.html"
+    }),
+    new webpack.ProvidePlugin({
+      React: "react",
+      $: "jquery",
+      ReactDOM: "react-dom"
+    }),
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify(
+        process.env.NODE_ENV || "development"
+      ),
+      "process.env.BROWSER": JSON.stringify(true)
+    })
+  ],
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, "src"),
+      assets: path.resolve(__dirname, "src/assets"),
+      client: path.resolve(__dirname, "src/client"),
+      server: path.resolve(__dirname, "src/server")
     }
-}
+  },
+  node: {
+    module: "empty",
+    fs: "empty"
+  }
+};
--- webpack.config.server.js
+++ webpack.config.server.js
@@ -1,83 +1,86 @@
-const nodeExternals = require('webpack-node-externals'),
-    path = require('path'),
-    srcPath = path.resolve(__dirname),
-    distPath = path.resolve(__dirname, 'dist'),
-    ExtractTextPlugin = require("extract-text-webpack-plugin");
+const nodeExternals = require("webpack-node-externals"),
+  path = require("path"),
+  srcPath = path.resolve(__dirname),
+  distPath = path.resolve(__dirname, "dist"),
+  ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-const production = process.env.NODE_ENV &&
-    process.env.NODE_ENV === "production";
-const Dotenv = require('dotenv-webpack');
+const production =
+  process.env.NODE_ENV && process.env.NODE_ENV === "production";
+const Dotenv = require("dotenv-webpack");
 if (!production) {
-    require('dotenv').config();
+  require("dotenv").config();
 }
 
-const modeWebpack = production ?
-    "production" :
-    "development";
+const modeWebpack = production ? "production" : "development";
 
 module.exports = {
-    context: srcPath,
-    entry: './src/server/server.js',
-    output: {
-        path: distPath,
-        filename: 'server.js',
-        publicPath: '/dist/'
-    },
-    target: 'node',
-    mode: modeWebpack,
-    node: {
-        __dirname: false,
-        __filename: false
-    },
-    module: {
-        rules: [{
-                test: /\.(js|jsx)$/,
-                exclude: [/node_modules/],
-                use: [{
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['es2015', 'react'],
-                        plugins: ['transform-object-rest-spread']
-                    }
-                }],
-            }, {
-                test: /\.scss$/,
-                loader: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: ["css-loader", "sass-loader"]
-                })
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: ["file-loader"]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: ["file-loader"]
+  context: srcPath,
+  entry: "./src/server/server.js",
+  output: {
+    path: distPath,
+    filename: "server.js",
+    publicPath: "/dist/"
+  },
+  target: "node",
+  mode: modeWebpack,
+  node: {
+    __dirname: false,
+    __filename: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: [/node_modules/],
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["es2015", "react"],
+              plugins: ["transform-object-rest-spread"]
             }
-            //loaders for other file types can go here
+          }
         ]
-    },
-    plugins: [
-        new Dotenv(),
-        new ExtractTextPlugin({
-            filename: "app.css"
-        }),
-        // new HtmlWebPackPlugin({
-        //     template: "./src/index.html",
-        //     filename: "./index.html"
-        // }),
-        // new webpack.DefinePlugin({
-        //     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
-        // }),
-    ],
-    resolve: {
-        alias: {
-            src: path.resolve(__dirname, "src"),
-            assets: path.resolve(__dirname, "src/assets"),
-            client: path.resolve(__dirname, "src/client"),
-        }
-    },
-    externals: nodeExternals(),
-    devtool: 'source-map'
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: ["css-loader", "sass-loader"]
+        })
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"]
+      }
+      //loaders for other file types can go here
+    ]
+  },
+  plugins: [
+    new Dotenv(),
+    new ExtractTextPlugin({
+      filename: "app.css"
+    })
+    // new HtmlWebPackPlugin({
+    //     template: "./src/index.html",
+    //     filename: "./index.html"
+    // }),
+    // new webpack.DefinePlugin({
+    //     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+    // }),
+  ],
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, "src"),
+      assets: path.resolve(__dirname, "src/assets"),
+      client: path.resolve(__dirname, "src/client"),
+      server: path.resolve(__dirname, "src/server")
+    }
+  },
+  externals: nodeExternals(),
+  devtool: "source-map"
 };
