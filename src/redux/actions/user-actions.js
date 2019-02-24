f62121918bdef4d201b2b2d453fd4831ec704b60
saman <saman.ashrafi10@gmail.com>
2019-02-24 23:51:05 +0330

    [Task] Complate Login action

--- src/redux/actions/user-actions.js
+++ src/redux/actions/user-actions.js
@@ -1,5 +1,11 @@
 import { Types } from "src/redux/constants/user-types";
-import Request from "axios";
+import { User } from "src/redux/constants/types";
+import { apiUrl } from "src/client/routes/apiUrl.js";
+import { setAuthToken } from "client/app/common/setAuthToken";
+
+import jwt_decode from "jwt-decode";
+
+import Req from "axios";
 
 export function getName(id) {
   return async function(dispatch, getState) {
@@ -8,9 +14,52 @@ export function getName(id) {
   };
 }
 function getUserFromAPI(id) {
-  return Request.get(`https://jsonplaceholder.typicode.com/users/${id}`);
+  return Req.get(`https://jsonplaceholder.typicode.com/users/${id}`);
 }
 
 export function registerUser() {}
 
-export function loginUser() {}
+export function loginUser(userData) {
+  console.log("login user is done");
+
+  return async function(dispatch) {
+    dispatch({
+      type: User.USERLOGIN_LOAD,
+      isAuthenticated: false
+    });
+    await getUser(userData, dispatch);
+  };
+}
+
+function getUser(userData, dispatch) {
+  // addClassById("btn-login", "clicked");
+  console.log("getUser");
+
+  return Req.post(apiUrl + "users/login", userData)
+    .then(res => {
+      // save to localStroge
+      const { token } = res.data;
+      //set token to Is
+      localStorage.setItem("jwtToken", token);
+      // Set Token to Auth header
+      setAuthToken(token);
+      // decode token to get user data
+      const decoded = jwt_decode(token);
+      // set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err => {
+      // removeClassById("btn-login", "clicked");
+      dispatch({
+        type: User.USER_ERRORS,
+        payload: err.response.data
+      });
+    });
+}
+
+export const setCurrentUser = decoded => {
+  return {
+    type: User.USERLOGIN_SUCCESS,
+    payload: decoded
+  };
+};
--- src/redux/reducers/user-reducer.js
+++ src/redux/reducers/user-reducer.js
@@ -5,7 +5,7 @@ const initialState = {
   email: null,
   isAuthenticated: false,
   user: {},
-  errors: []
+  errors: {}
 };
 export default function userReducer(state = initialState, action) {
   switch (action.type) {
@@ -18,9 +18,14 @@ export default function userReducer(state = initialState, action) {
     case User.USERLOGIN_LOAD:
       return { ...state, isAuthenticated: false };
     case User.USERLOGIN_SUCCESS:
-      return { ...state, isAuthenticated: true, user: action.payload };
-    case User.USERLOGIN_ERRORS:
-      return { ...state, errors: action.payload.errors };
+      return {
+        ...state,
+        isAuthenticated: true,
+        user: action.payload,
+        errors: {}
+      };
+    case User.USER_ERRORS:
+      return { ...state, errors: action.payload };
     default:
       return state;
   }
