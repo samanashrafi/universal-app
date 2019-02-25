9db21401207f2a6e792ede0774d374d69df6ee4e
saman <saman.ashrafi10@gmail.com>
2019-02-25 23:40:01 +0330

    [Task] Change api url

--- src/redux/actions/category-actions.js
+++ src/redux/actions/category-actions.js
@@ -1,6 +1,6 @@
 import Req from "axios";
 import { Category } from "src/redux/constants/types";
-import { apiUrl } from "src/client/routes/apiUrl.js";
+import { jsonUrl } from "src/client/routes/apiUrl.js";
 
 export function getCategory() {
   return async function(dispatch) {
@@ -8,7 +8,7 @@ export function getCategory() {
       type: Category.CATEGORY_LOAD,
       isLoaded: false
     });
-    let { data } = await Req.get(apiUrl + "category");
+    let { data } = await Req.get(jsonUrl + "category");
     dispatch({
       type: Category.CATEGORY_SUCCESS,
       payload: data,
