6f882cc41c888c7bf43450217e7febb73c0a4db5
saman <saman.ashrafi10@gmail.com>
2019-02-25 23:39:42 +0330

    [Task] Add jsonUrl

--- src/client/routes/apiUrl.js
+++ src/client/routes/apiUrl.js
@@ -1,10 +1,12 @@
-const production = process.env.NODE_ENV &&
-    process.env.NODE_ENV === "production";
+const production =
+  process.env.NODE_ENV && process.env.NODE_ENV === "production";
 
 if (!production) {
-    require('dotenv').config();
+  require("dotenv").config();
 }
 
-export const apiUrl = production ?
-    process.env.PROD_RESTURL :
-    process.env.JSONSERVER_RESTURL;
+export const apiUrl = production
+  ? process.env.PROD_RESTURL
+  : process.env.JSONSERVER_RESTURL;
+
+export const jsonUrl = "http://localhost:4000/api/";
