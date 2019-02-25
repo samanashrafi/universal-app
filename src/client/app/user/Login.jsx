23c532d4f7dcadee1bf872a2c63d064c64080acf
saman <saman.ashrafi10@gmail.com>
2019-02-25 10:03:13 +0330

    [Task] Complate Login from  & actions & vaidations

--- src/client/app/user/Login.jsx
+++ src/client/app/user/Login.jsx
@@ -5,6 +5,8 @@ import { PropTypes } from "prop-types";
 import { withRouter } from "react-router-dom";
 import { Helmet } from "react-helmet";
 import { loginUser } from "src/redux/actions/user-actions.js";
+import { isEmpty } from "client/app/common/helpers.js";
+import Validator from "validator";
 import TextFieldGroup from "client/app/common/TextFieldGroup.js";
 
 class Login extends Component {
@@ -13,23 +15,73 @@ class Login extends Component {
     this.state = {
       email: "",
       password: "",
-      errors: []
+      errors: {}
     };
+    this.errors = {};
+    this.isValid = false;
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
   }
 
-  onChange(e) {
-    this.setState({ [e.target.name]: [e.target.value] });
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
   }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+
+  isEmail(state, field, msg) {
+    let v = Validator.isEmail(state);
+    if (v == false) {
+      this.errors[field] = msg;
+    }
+  }
+  isEmpty(state, field, msg) {
+    let v = Validator.isEmpty(state);
+    if (v) {
+      this.errors[field] = msg;
+    }
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
   onSubmit(e) {
-    e.preventDefualt();
-    const { email, password } = this.state;
-    const data = {
-      email: email,
-      password: password
-    };
-    this.props.loginUser(data);
+    e.preventDefault();
+    // debugger;
+    let { email, password } = this.state;
+
+    this.isEmpty(email, "email", "لطفا ایمیل خود را وارد فرمایید...");
+    this.isEmpty(password, "password", "لطفا کلمه عبور خود را وارد فرمایید...");
+    if (!isEmpty(email)) {
+      this.isEmail(email, "email", "لطفا ایمیل را با فرمت صحیح وارد کنید...");
+    }
+    this.setState({
+      errors: this.errors
+    });
+    if (Object.values(this.errors).length == 0) {
+      this.isValid = true;
+    }
+
+    this.errors = {};
+
+    if (this.isValid) {
+      const data = {
+        email: email,
+        password: password
+      };
+      this.props.loginUser(data);
+    }
   }
   render() {
     const { email, password, errors } = this.state;
@@ -52,15 +104,17 @@ class Login extends Component {
                 name="email"
                 label="ایمیل"
                 icon={"k-mail"}
+                className="ltr"
                 value={email}
                 onChange={this.onChange}
                 error={errors.email}
               />
               <TextFieldGroup
-                type="text"
+                type="password"
                 name="password"
                 label="کلمه عبور"
                 icon={"k-lock-1"}
+                className="ltr"
                 value={password}
                 onChange={this.onChange}
                 error={errors.password}
@@ -90,7 +144,7 @@ class Login extends Component {
 Login.propTypes = {
   loginUser: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
-  errors: PropTypes.array.isRequired
+  errors: PropTypes.object.isRequired
 };
 const mapStateToProps = state => ({
   auth: state.auth,
